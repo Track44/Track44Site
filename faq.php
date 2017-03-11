ba4bfb980418dc8fd635dde072e79db9440e7992
UnbendableStraw <nicsplatts@gmail.com>
2017-03-11 01:06:45 -0800

    UNfinished for testing

---
+++ faq.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<!-- Spaghetti code by UnbendableStraw, poncethecat and fearnlj01. some code was used from html5 up, css3 please!, and ihatetomatoes -->
+<!-- Someone mention Fearn so I can find out if people ever read the commit logs -->
+<html>
+	<head>
+		<title>Track 44</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<!-- [if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif] -->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/jquery.scrolly.min.js"></script>
+		<script src="js/jquery.poptrox.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/init.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel.css" />
+			<link rel="stylesheet" href="css/style.css" />
+		</noscript>
+		<!-- [if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif] -->
+	</head>
+
+<!--	<style>
+		body:not(.loaded) {
+			background-color: #2E413F;
+		}
+
+		body:not(.loaded) > *:not(#loader-wrapper) {
+			display: none !important;
+		}
+	</style>
+
+	<body class="is-loading"> -->
+		<!-- Demi please fix this damn Navbar for mobile smh -->
+		<div class="bar" id="navbar">
+			<div class="insidebar">
+				||
+				<a href="/about/" class="button style4">About Us</a>
+				<a href="/mail/" class="button style4">Mail</a>
+				<a href="/colors/" class="button style4">Colors</a>
+			</div>
+		</div>
+
+		<!-- Header -->
+
+		<div id="loader-wrapper">
+			<div id="loader"></div>
+			<div class="loader-section section-left"></div>
+			<div class="loader-section section-right"></div>
+		</div>
+
+		<div id="content">
+			<section id="header">
+				<header>
+					<h1>Track 44</h1>
+					<p>Community Information and FAQ</p>
+				</header>
+				<footer style="bottom: 6em;">
+					<a href="#banner" class="button style2 scrolly-middle">eggs dee</a>
+				</footer>
+			</section>
+		</div>
+
+		<!-- Banner -->
+			<section id="banner">
+				<header>
+					<h2></h2>
+				</header>
+				<p class="white">As a Track 44 member, you get access to a lot of cool things. Dedicated game servers, music and fun bots, abusive admins ;), a ton of text and voice channels, your own email account, and more!</a></p>
+
+			</section>
+
+			</article>
+
+			<!--  Channel Descriptions -->
+
+			<article class="container box style2">
+				<header>
+					<h2>Channel Descriptions</h2>
+					<p>#welcome is the <b>safe for work</b> default channel that shows when you join. It outlines the basics.</p>
+					<p>CS:GO</p>
+					<p>CS1.6</p>
+					<a href="videos/notfunnytbh.mp4" target="blank">
+						<div style="padding: 10px 10px;">
+						</div>
+					</a>
+				</header>
+			</article>
+
+		<!-- Coming Soon -->
+
+
+		<article class="container box style2" id="Mishiki">
+			<header>
+				<div class="3u" style="float: left;margin-right: 2%;">
+					<a href="images/portfolio/mishiki.jpg" class="image fit">
+						<img src="images/portfolio/mishiki.jpg" alt="Mishiki" title="Mishiki">
+					</a>
+				</div>
+				<div>
+					<h2>Mishiki</h2>
+					<p>Mishiki helps test the hosted servers before changes are made public. He also maintains the Team Fortress 2 server, and moderates the discord server, as well as getting people to join the Minecraft and Garry's Mod servers.</p>
+				</div>
+			</header>
+		</article>
+
+		<p>
+			<a title="hit counter">
+			Vistor Count: <?php include "/var/www/count.php"; ?>
+			</a>
+		</p>
+		<p>
+			<center>
+					<img src="https://i.imgur.com/nPxjS4J.gif" />
+					<p style="font-family:courier; font-size:60%;">Cursed Image &copy; Maria 2017</p>
+				</center>
+		</p>
+		<p>
+			<audio id="music" controls>
+			<source src="Track 44.mp3" type="audio/mpeg">
+			</audio>
+		</p>
+	</body>
+</html>
